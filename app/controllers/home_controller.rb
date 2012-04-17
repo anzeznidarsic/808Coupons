e40673b6702550128a9a3238bc55c34ad93ba678
Anze Znidarsic <a@anze.com>
2012-04-17 13:55:53 -1000

    Added default date format. Finished manual location search.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  
   http_basic_authenticate_with :name => 'aa', :password => 'bb'
   
   layout :get_layout
@@ -12,6 +13,9 @@ class HomeController < ApplicationController
     lat = params[:lat]
     lon = params[:lon]
     @companies = Company.near([lat, lon], 10)
+    @companies.each { |c|
+      c.distance = c.distance.to_d.round(2)
+    }
   end
   
   # List of coupons for one particular company
@@ -28,20 +32,24 @@ class HomeController < ApplicationController
   def error_location
   end
   
+  # Search manually for location through text input
   def search_location
-    @s = Geocoder.search(params[:location])
+    @location = params[:location]
+    s = Geocoder.search(@location)
 
-    return false
-    lat = s.latitude
-    lon = s.longitude
+    if (s[0])
+      lat = s[0].latitude
+      lon = s[0].longitude
+    end
+    
     if (lat && lon)
-      redirect_to '/home/nearby?lat='+lat.to_s+'&lon='+lon.to_s
+       redirect_to '/home/nearby?lat='+lat.to_s+'&lon='+lon.to_s
     end
   end
   
   private
   
-  # Set layour (website can work as ajax or fullsite)
+  # Set layout (website can work as ajax or fullsite)
   def get_layout
     @layout = request.xhr? ? nil : 'front'
     @layout
