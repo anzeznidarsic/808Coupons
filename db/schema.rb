ad6cf79c07d9feb0f640de79185bd560de05348e
Anze Znidarsic <a@anze.com>
2012-05-08 14:35:17 -1000

    Added comment form field to companies. Changed year range from 1950 for year of when company was opened.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120419001511) do
+ActiveRecord::Schema.define(:version => 20120509002653) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -46,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20120419001511) do
     t.datetime "updated_at",                           :null => false
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "comment"
   end
 
   create_table "coupons", :force => true do |t|
